b2a1f380a69ca0e2d0bfc144965bf828db8b9aab
Amauri Souza <amauri.teixeira@kognit.com.br>
2019-01-14 11:48:10 -0200

    FEATURE - add files project

---
+++ App.js
@@ -0,0 +1,178 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet
+} from "react-native";
+
+class Inputs extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      number1: 0,
+      number2: 0,
+      result : ''
+    } 
+  }
+
+  handleNumber1 = text => this.setState({ number1: text });
+  handleNumber2 = text => this.setState({ number2: text });
+
+  add = (number1, number2) => {
+    this.setState({result: parseFloat(number1) + parseFloat(number2)}) 
+  };
+
+  multi = (number1, number2) => {
+    this.setState({result: parseFloat(number1) * parseFloat(number2)}) 
+  };
+
+  div = (number1, number2) => {
+    this.setState({result: parseFloat(number1) / parseFloat(number2)}) 
+  };
+
+  sub = (number1, number2) => {
+    this.setState({result: parseFloat(number1) - parseFloat(number2)}) 
+  };
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View
+          style={[
+            styles.flexContentBetween,
+            styles.box,
+            styles.marginBottomBox
+          ]}
+        >
+          <Text style={[styles.textInfo, styles.bold]}>Resultado</Text>
+          <Text style={styles.textInfo}>{this.state.result !== '' ? this.state.result.toFixed(1) : ''}
+            
+          </Text>
+        </View>
+
+        <View style={styles.box}>
+          <View style={styles.flexContentBetween}>
+            <TextInput
+              style={styles.inputText}
+              underlineColorAndroid="gray"
+              placeholder="Number 1"
+              autoCapitalize="none"
+              onChangeText={this.handleNumber1}
+            />
+
+            <TextInput
+              style={styles.inputText}
+              underlineColorAndroid="gray"
+              placeholder="Number 2"
+              autoCapitalize="none"
+              onChangeText={this.handleNumber2}
+            />
+          </View>
+
+          <View style={styles.flexContentBetween}>
+            <TouchableOpacity
+              style={[styles.buttonDefault, styles.operation]}
+              onPress={() => this.add(this.state.number1, this.state.number2)}
+            >
+              <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 20 }}>
+                +
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity 
+              style={[styles.buttonDefault, styles.operation]}
+              onPress={() => this.sub(this.state.number1, this.state.number2)}
+            >
+              <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 20 }}>
+                -
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity 
+              style={[styles.buttonDefault, styles.operation]}
+              onPress={() => this.multi(this.state.number1, this.state.number2)}
+            >
+              <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 20 }}>
+                *
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity 
+             style={[styles.buttonDefault, styles.operation]}
+             onPress={() => this.div(this.state.number1, this.state.number2)}
+            >
+              <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 20 }}>
+                /
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity 
+             style={[styles.buttonDefault, styles.delete]}
+            >
+              <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 20 }}>
+                x
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+export default Inputs;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15
+  },
+
+  flexContentBetween: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+
+  box: {
+    elevation: 2,
+    borderRadius: 5,
+    backgroundColor: "#fff",
+    padding: 10
+  },
+
+  textInfo: {
+    fontSize: 18,
+    color: "gray"
+  },
+
+  bold: {
+    fontWeight: "bold"
+  },
+
+  inputText: {
+    flex: 1
+  },
+
+  marginBottomBox: {
+    marginBottom: 10
+  },
+
+  operation: {
+    backgroundColor: "#4181ee",
+  },
+
+  delete: {
+    backgroundColor: "red",
+  },
+
+
+  buttonDefault: {
+    borderRadius: 20,
+    width: 40,
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 10
+  }
+});
